c9d2bc7d676b434d18a414e02aec706226670ecf
adirak <adirak@gmail.com>
2020-09-26 16:40:42 +0700

    update trigger

---
+++ apis/activity/activityQueueTrigger.go
@@ -0,0 +1,98 @@
+// Package activity contains an apis for activity data
+package activity
+
+import (
+	"log"
+
+	"github.com/adirak/app-385-core/data"
+	"github.com/adirak/app-385-core/dbexec"
+	"github.com/adirak/app-385-core/strava"
+)
+
+// TriggerActivityQueue is api function to triger strava query from its api
+func TriggerActivityQueue(reqt data.ReqtData) (resp data.RespData) {
+
+	// Output data
+	outData := make(map[string]interface{})
+
+	// Get Data from activity queue
+	queue, err := dbexec.LoadActivityQueue()
+
+	if err == nil {
+
+		// Loop Get Activity
+		loopToGetStravaActivity(queue)
+
+		// Looping success
+		resp.Success = true
+	}
+
+	// Footer response
+	resp.OutData = outData
+	resp.Err = err
+	return resp
+}
+
+// loopToGetStravaActivity is function loop to get Strava Activity
+func loopToGetStravaActivity(queue []data.ActivityQueue) {
+
+	// Log
+	log.Println("**loopToGetStravaActivity Start")
+	defer log.Println("**loopToGetStravaActivity End")
+
+	// Looping
+	for _, actQ := range queue {
+
+		if actQ.Active {
+
+			// Query Strava User Token
+			token, err := dbexec.LoadStravaUserToken(actQ.StravaUserID)
+			if err == nil && token.AppID > 0 {
+
+				// Query Strava App
+				app, err := dbexec.LoadStravaApp(token.AppID)
+				if err == nil {
+
+					// Call to request activity from Strava web
+					result, rToken, err := strava.LoadStravaActivity(token, app)
+					if err == nil && rToken.TokenExpired == false {
+
+						// Query Strava Activity History
+						history, err := dbexec.LoadStravaActivityHistory(token.UserID)
+
+						if err == nil {
+
+							updateCount, err := calulateEventProgress(result, history, token)
+
+							if err == nil && updateCount > 0 {
+								dbexec.UpdateActivityQueue(actQ)
+							}
+
+						} else {
+							log.Println("cannot loadStravaActivityHistory by user_id : ", token.UserID, " --> ", err.Error())
+						}
+
+					} else {
+
+						// To update Strava User Token by active = false
+						err = dbexec.UpdateTokenExpired(token)
+						if err != nil {
+							log.Println("cannot updateTokenExpired by user_id : ", token.UserID, " --> ", err.Error())
+						}
+
+					}
+
+				} else {
+					log.Println("cannot loadStravaApp by app_id : ", token.AppID, " --> ", err.Error())
+				}
+
+			} else {
+				log.Println("cannot loadStravaUserToken by strava_user_id : ", actQ.StravaUserID, " --> ", err.Error())
+			}
+
+		} else {
+			log.Println("strava_user_id : ", actQ.StravaUserID, " is not active")
+		}
+	}
+
+}
---
+++ apis/activity/calculateEventProgress.go
@@ -0,0 +1,110 @@
+// Package activity contains an apis for activity data
+package activity
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/adirak/app-385-core/data"
+	"github.com/adirak/app-385-core/dbexec"
+)
+
+// calulateEventProgress is function to calulate EventProgross by activity
+func calulateEventProgress(actData map[string]interface{}, history data.StravaActivityHistory, token data.StravaUserToken) (updateCount int, err error) {
+
+	activities, ok := actData["activities"].([]map[string]interface{})
+	updateCount = 0
+	if ok {
+
+		hText := history.History
+
+		// Looping
+		for _, act := range activities {
+
+			ID := act["id"].(float64)
+			intID := int(ID)
+
+			// Checking new ID with history because user cannot add old ID of activity
+			if !strings.Contains(hText, fmt.Sprint(intID)) {
+
+				distance := act["distance"].(float64)
+				elapsedTime := act["elapsed_time"].(float64)
+				//manual := act["manual"].(bool)
+
+				validDistance := getValidDistance(distance)
+				validElapsedTime := getValidElapsedTime(elapsedTime)
+
+				// Load Event Progress from event_progress table
+				listEventProg, err := dbexec.LoadEventProgress(token.UserID)
+				var errUpdate error
+				if err == nil && listEventProg != nil {
+
+					for _, eventProg := range listEventProg {
+
+						// Ignore if event progress is not active
+						if eventProg.Active {
+
+							// Increase data in event_progress
+							eventProg.Distance = eventProg.Distance + distance
+							eventProg.ElapsedTime = eventProg.ElapsedTime + elapsedTime
+							eventProg.ValidDistance = eventProg.ValidDistance + validDistance
+							eventProg.ValidElapsedTime = eventProg.ValidElapsedTime + validElapsedTime
+
+							// Update eventProgress to database
+							errUpdate = dbexec.UpdateEventProgress(eventProg)
+							if errUpdate != nil {
+								log.Println("Cannot update event_progress user_id=", eventProg.UserID, " event_id=", eventProg.EventID)
+								break
+							}
+						}
+
+					}
+
+					if errUpdate == nil {
+						hText = fmt.Sprint(intID) + "," + hText
+					} else {
+						updateCount++
+					}
+				}
+
+			} else {
+				updateCount++
+			}
+		}
+
+		// Length limit
+		if len(hText) > 300 {
+			hText = hText[:150]
+		}
+
+		// Update Strava Activity History
+		history.History = hText
+		if history.HasHistory {
+			err := dbexec.UpdateStravaActivityHistory(history)
+			if err != nil {
+				log.Println("updateStravaActivityHistory fail at user_id=", history.UserID)
+			}
+		} else {
+			err := dbexec.InsertStravaActivityHistory(history)
+			if err != nil {
+				log.Println("insertStravaActivityHistory fail at user_id=", history.UserID)
+			}
+		}
+
+	} else {
+		log.Println("Cannot cast activities to array : ", actData)
+	}
+
+	return
+}
+
+// getValidDistance to get valid distance by rule
+func getValidDistance(distance float64) float64 {
+	return distance
+}
+
+// getValidElapsedTime to get elapsed time by rule
+func getValidElapsedTime(elapsedTime float64) float64 {
+	return elapsedTime
+}
